4ba57790a769238a06c4bc048e1c56254c16d10c
The Device <thomasuprety135@gmail.com>
2024-05-10 08:27:48 +0545

    Initial Push

---
+++ src/index.js
@@ -0,0 +1,45 @@
+let accordian = document.getElementsByClassName("FAQ__title");
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", function () {
+    if (this.childNodes[1].classList.contains("fa-plus")) {
+      this.childNodes[1].classList.remove("fa-plus");
+      this.childNodes[1].classList.add("fa-times");
+    } else {
+      this.childNodes[1].classList.remove("fa-times");
+      this.childNodes[1].classList.add("fa-plus");
+    }
+
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
+// form mail post request
+//get the form by its id
+const form = document.getElementById("contact-form");
+
+//1.
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //2.
+  let mail = new FormData(form);
+
+  //3.
+  sendMail(mail);
+});
+
+const sendMail = (mail) => {
+  //1.
+  fetch("https://nodemailer-vic-lo.herokuapp.com/send", {
+    method: "post", //2.
+    body: mail, //3.
+  }).then((response) => {
+    return response.json();
+  });
+};
